118f7b99db34930d32aff0638389685722d8cf72
Marc Anell <marc.anell@pilaf.safelayer.lan>
2015-12-09 12:02:27 +0100

    first commit

---
+++ troughput_test.go
@@ -0,0 +1,31 @@
+package troughput
+
+import (
+	"net/http"
+	"testing"
+	"github.com/codegangsta/negroni"
+	"net/http/httptest"
+)
+
+func TestTroughput(t *testing.T) {
+	tp := NewTroughput()
+	
+	n := negroni.New()
+	n.Use(tp)
+	n.UseHandler(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusNotFound)
+	}))
+	
+	req, err := http.NewRequest("GET", "http://localhost:3000/foobar", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	
+	recorder := httptest.NewRecorder()
+
+	n.ServeHTTP(recorder, req)
+	
+	if recorder.Code != http.StatusNotFound {
+		t.Fatal("Invalid status code")
+	}
+}
