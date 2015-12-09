d36db97e731addfa105bd814c41b7e96da644cfe
Marc Anell <marc.anell@pilaf.safelayer.lan>
2015-12-09 12:18:44 +0100

    Go fmt

--- troughput.go
+++ troughput.go
@@ -20,41 +20,41 @@ type Troughput struct {
 // NewTroughput return a new Troughput instance
 func NewTroughput() *Troughput {
 	stats := make(chan *Stat, 16384)
-	
+
 	tr := &Troughput{
 		stats: stats,
 	}
-	
+
 	go tr.Log()
-	
+
 	return tr
 }
 
 // Log prints stats periodically
 func (t *Troughput) Log() {
 	stats := make(map[string]int, 256)
-	
+
 	tnext := time.Now().Add(1 * time.Second).Round(time.Second)
 	for {
-		stat := <- t.stats
-		
+		stat := <-t.stats
+
 		stats[stat.request]++
-		
+
 		// Print info, clear data, and reset the timer
 		if time.Now().After(tnext) {
 			for k, v := range stats {
 				fmt.Printf("%s   %d TPS \n", k, v)
 				delete(stats, k)
 			}
-			
+
 			tnext = time.Now().Add(1 * time.Second).Round(time.Second)
 		}
-		
+
 	}
 }
 
 func (t *Troughput) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	next(w, r)
-	
+
 	t.stats <- &Stat{r.Method + " " + r.RequestURI}
 }
--- troughput_test.go
+++ troughput_test.go
@@ -1,30 +1,30 @@
 package troughput
 
 import (
-	"net/http"
-	"testing"
 	"github.com/codegangsta/negroni"
+	"net/http"
 	"net/http/httptest"
+	"testing"
 )
 
 func TestTroughput(t *testing.T) {
 	tp := NewTroughput()
-	
+
 	n := negroni.New()
 	n.Use(tp)
 	n.UseHandler(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusNotFound)
 	}))
-	
+
 	req, err := http.NewRequest("GET", "http://localhost:3000/foobar", nil)
 	if err != nil {
 		t.Error(err)
 	}
-	
+
 	recorder := httptest.NewRecorder()
 
 	n.ServeHTTP(recorder, req)
-	
+
 	if recorder.Code != http.StatusNotFound {
 		t.Fatal("Invalid status code")
 	}
